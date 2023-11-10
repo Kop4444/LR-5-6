926e7b4195c7c59ccbd5129258b4e0243bc7cb27
Kop4444 <144805490+Kop4444@users.noreply.github.com>
2023-11-10 14:35:17 +0300

    Add files via upload

---
+++ LR 5-6 1.3.pas
@@ -0,0 +1,48 @@
+﻿{3.	Сформировать одномерный массив из 20 целых чисел из диапазона [-52,65] 
+случайным образом. Найти наибольший элемент массива и его номер (при условии, что все элементы массива различны). 
+}
+
+
+var a:array[1..20] of integer;
+i: integer;
+s: integer;
+x: integer;
+y:integer;
+x1:integer;
+begin 
+  s := 20;
+  x1:=-52;
+  x :=65;
+  y:=0;
+  
+  for i:=1 to s do 
+    a[i] := Random(-52,65);
+  for i:=1 to s do
+    write(a[i],' ');
+  
+  for i:= 1 to s do
+    if (a[i] > x1) and (a[i]>0) then begin
+      x1:= a[i];
+      y:=i;
+      end;
+
+writeln('итог',x1,' ',y); 
+ 
+  for i:= 1 to s do
+    if (a[i] < x) and (a[i]>0) then begin
+      x:= a[i];
+      y:=i;
+      end;
+
+writeln('итог',x,' ',y); 
+      
+  for i:= 1 to s do
+    if a[i] mod 5 = 0 then begin
+      x:= a[i];
+      y:=i;
+      end;
+        
+writeln('итог',x,' ',y);    
+end.
+
+
